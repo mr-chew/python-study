e167d1fea1c96877a55cdfcdeddd71370890b8ff
chew <mr.krabs.6868@protonmail.com>
2021-11-14 23:13:15 +0800

    Initial commit

---
+++ python-basics.py
@@ -0,0 +1,51 @@
+#Python Types
+#Strings
+print('#Strings#')
+Spam = 'abcd'
+#print a, d
+print(Spam[0])
+print(Spam[3])
+
+
+# print True
+print('a' in Spam)
+
+#print False
+print('a' not in Spam)
+
+#print 4, which is the length of the string
+print(len(Spam))
+
+#Numbers
+print('\n#Numbers#')
+Spam = 2.5
+Egg = 2
+
+#print 5
+print(Spam * Egg)
+
+#print 1.25
+print(Spam / Egg)
+
+#print 0 (y//x) = Floor(y/x) 
+print(Egg // Spam)
+
+#Boolean
+print('\n#Boolean#')
+Spam = True
+Egg = False
+
+print(Spam)
+print(Egg)
+
+#List
+print('\n#List#')
+Spam = [10, 25, 63, 104]
+Egg = ['a', 'q', 'blah']
+
+#print 10, q
+print(Spam[0])
+print(Egg[1])
+
+Spam.append(22)
+print(Spam)
