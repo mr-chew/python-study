89fed223f132b381375b7e36999c1e3c7bdb2c84
chew <mr.krabs.6868@protonmail.com>
2021-11-17 02:59:40 +0800

    .

--- python-basics.py
+++ python-basics.py
@@ -1,57 +1,58 @@
 #Python Types
 #Strings
 print('#Strings#')
-Spam = 'abcd'
+spam = 'abcd'
 #print a, d
-print(Spam[0])
-print(Spam[3])
+print(spam[0])
+print(spam[3])
 
 
 # print True
-print('a' in Spam)
+print('a' in spam)
 
 #print False
-print('a' not in Spam)
+print('a' not in spam)
 
 #print 4, which is the length of the string
-print(len(Spam))
+print(len(spam))
 
 #Numbers
 print('\n#Numbers#')
-Spam = 2.5
-Egg = 2
+spam = 2.5
+egg = 2
 
 #print 5
-print(Spam * Egg)
+print(spam * egg)
 
 #print 1.25
-print(Spam / Egg)
+print(spam / egg)
 
 #print 0 (y//x) = Floor(y/x) 
-print(Egg // Spam)
+print(egg // spam)
 
 #Boolean
 print('\n#Boolean#')
-Spam = True
-Egg = False
+spam = True
+egg = False
 
-print(Spam)
-print(Egg)
+print(spam)
+print(egg)
 
 #List
-print('\n#List#')
-Spam = [10, 25, 63, 104]
-Egg = ['a', 'q', 'blah']
+print('\n#List# is a collection which is ordered and changeable. Allows duplicate members.')
+spam = [10, 25, 63, 104]
+egg = ['a', 'q', 'blah']
 
 #print 10, q
-print(Spam[0])
-print(Egg[1])
+print(spam[0])
+print(egg[1])
 
-Spam.append(22)
-print(Spam)
+spam.append(22)
+print(spam)
 
 #Dictionary
-print("\n#Dictionary#")
+print('\n#Dictionary# is a collection which is ordered** and changeable. No duplicate members.')
+print('**As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.\n')
 _dict = {
     'a': 'Down by the bay',
     'b': 'where the watermelon grows'
@@ -73,4 +74,33 @@ _dict['c']= 'back to my home'
 print(_dict['c'])
 
 #Tuples
-print("\n#Tupples")
+print('\nTupples# is a collection which is ordered and unchangeable. Allows duplicate members.')
+
+spam =('a', 'b')
+egg = ('c', 'd')
+
+#add tupples and print ('a', 'b', 'c', 'd')
+print(spam+egg)
+
+_list = [5,6,7,8]
+_tuple = (_list)
+print(min(_tuple))
+print(max(_tuple))
+print(len(_tuple))
+
+#Sets
+print('\nSets is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.')
+print('*Set items are unchangeable, but you can remove and/or add items whenever you like.\n')
+
+#convert list to set
+spam =  ['a','a','b','c', 'c']
+spam = set(spam)
+print(spam)
+
+spam.add('d')
+spam.add('e')
+print(spam)
+
+spam.remove('c')
+spam.remove('e')
+print(spam)
