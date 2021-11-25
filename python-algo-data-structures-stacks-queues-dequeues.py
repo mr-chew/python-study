42389f403c3fbfa0e57bab1ffd54a0a8c5070cc8
chew <mr.krabs.6868@protonmail.com>
2021-11-25 12:07:01 +0800

    .

--- python-algo-data-structures-stacks-queues-dequeues.py
+++ python-algo-data-structures-stacks-queues-dequeues.py
@@ -77,3 +77,68 @@ def last():
     return queue[-1]
 
 queue = ['spam', 'egg', 'ham', 'coffee', 'tea']
+print(f'starting queue is \"{queue}\" with size {size()}')
+print(f'\nthe first element is {first()} and the last element is {last()}')
+
+add('milk')
+print(f'\nIf we \'add\' the element milk, the queue is now {queue} with size of {size()} and the last element is {last()}')
+
+pop()
+print(f'\nif we pop from the queue, the first element is now {first()}')
+print(f'\nis the Queue empty? the answer is {empty()}')
+
+for i in range(size()):
+    pop()
+    
+print(f'if we pop the queue 5 times, the queue should be emptu now and is {empty()}')
+
+print('\n#Dequeue#')
+#append - add to the right
+#appendleft - add to the left
+#pop - remove an element from the right hand side
+#popleft - remove an element from the left hand side
+#size - check the length or size of the queue
+#empty - check if it’s empty
+
+dq = []
+
+def append(a):
+    dq.append(a)
+
+def appendleft(a):
+    dq.insert(0, a)
+
+def pop():
+    try:
+        del dq[-1]
+    except:
+        return "Dequeue is empty"
+
+def popleft():
+    try:
+        del dq[0]
+    except:
+        return "Dequeue is empty"
+
+def size():
+    return len(dq)
+
+def empty():
+    return True if len(dq) == 0 else False
+
+dq = ['left', 'inner left', 'middle', 'inner right', 'right']
+print(f"The current \"dq\" is {dq}")
+pop()
+popleft()
+print(f"\nAfter popping an element on both ends, the new dq is {dq} with a size of {size()}")
+
+append("new right")
+appendleft("new left")
+print(f"\nAppend new elements on both end, the new dq is {dq} with a size of {size()}")
+
+for i in range(size()):
+    pop()
+
+print(f"\nAfter all the values are pop, the dq is now {dq} and its now a size of {size()}")
+
+print(f"\ntry pop again -> {pop()}")
