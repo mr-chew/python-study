46958a5bd69829efc90d346f726821d42bb83ff5
chew <mr.krabs.6868@protonmail.com>
2021-11-29 23:10:07 +0800

    .

--- python-algo-data-structures-link-lists-binary-trees.py
+++ python-algo-data-structures-link-lists-binary-trees.py
@@ -1,1 +1,12 @@
-# https://www.geeksforgeeks.org/data-structures/linked-list/
+#create node
+class Node(object):
+    def __init__(self, data=None, next=None): #initialise node
+        self.data = data
+        self.next = next
+        
+    def get_data(self): #retrieve node data
+        return self.data
+    
+    def set_data(self):
+        self.data = new_data
+        
