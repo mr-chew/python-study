f1cf888c8bf6224967fb8698607a8f7759db7659
chew <mr.krabs.6868@protonmail.com>
2021-12-01 00:50:06 +0800

    .

--- python-algo-data-structures-link-lists-binary-trees.py
+++ python-algo-data-structures-link-lists-binary-trees.py
@@ -7,7 +7,7 @@ class Node(object):
     def get_data(self): #retrieve node data
         return self.data
     
-    def set_data(self): #set node data
+    def set_data(self, new_data): #set node data
         self.data = new_data
         
     def get_next(self): #get next node
@@ -49,11 +49,26 @@ class Linked_List(object):
         if currentNode.data == value: #if first node matches the value
             self.head = currentNode.next #set the head pointer to next node
         else:
-            while currentNode.next:
-            
-            
-        
-    
+            previousNode = currentNode #keeping track of previous node
+            currentNode = currentNode.next
+            while (currentNode): #loop through the linked list
+                if currentNode.data == value:                 
+                    self.size -= 1
+                    previousNode.next = currentNode.next #unlink the node from the linked list
+                    return currentNode
+                elif currentNode.next:
+                    previousNode = currentNode
+                    currentNode = currentNode.next
+                else:
+                    return ValueError("The value is not found")
+  
+    def traverse(self):
+        currentNode = self.head
+        arrayNode = [currentNode.data]
+        while currentNode.next:
+            currentNode = currentNode.next
+            arrayNode.append(currentNode.data)
+        return arrayNode
         
 node1 = Node("A")
 node2 = Node("Bb")
@@ -61,10 +76,20 @@ node3 = Node("Ccc")
 node4 = Node("Dddd")
 node5 = Node("Eeeee")
 
+
 ll = Linked_List(node1)
 ll.append(node2)
 ll.append(node3)
 ll.append(node4)
 ll.append(node5)
 
-#ll.delete("A")
+ll.delete('Eeeee')
+print(f"Current linked list is {ll.traverse()}")
+
+print(f"Now the data of node1 is {node1.get_data()}")
+node1.set_data("AAAAAAAA")
+print(f"Now the data of node1 is {node1.get_data()}")
+
+print(f"Now the size of the linked list is {ll.size}")
+print(f"If we search for Ccc111, we should see that it is not in the list: {ll.search('Ccc111')}")
+print(f"We should now get a response for searching for Ccc: {ll.search('Ccc')}")
